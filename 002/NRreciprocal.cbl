fa93ead66021b90775e2d09c0463cd0839e51b1f
Sam Williams <65340014+SamWilliams13@users.noreply.github.com>
2024-05-04 09:31:48 +0100

    modified to work on transpiler

--- 002/NRreciprocal.cbl
+++ 002/NRreciprocal.cbl
@@ -4,26 +4,24 @@
 
        DATA DIVISION.
            WORKING-STORAGE SECTION.
-           01  A     PIC S99      VALUE ZEROS.
-           01  XNEW  PIC S9V9(5)  VALUE ZEROS.
+           01  A     PIC S99      VALUE 0.
+           01  XNEW  PIC S9V9(5)  VALUE 0.
            01  XOLD  PIC S9V9(5)  VALUE -0.0100.
            01  N     PIC 99       VALUE 10.
-           01  INV   PIC S9V9(5)  VALUE ZEROS.
-      
-       PROCEDURE DIVISION.           
-      *> Get number 
+           01  INV   PIC S9V9(5)  VALUE 0.
+
+       PROCEDURE DIVISION.
+      *> Get number
            DISPLAY "Insert Number (-10<n<10)"
            ACCEPT A
               
       *> Check input     
            IF A<=-10 OR A >+10 THEN
-           DISPLAY "Error: number x must be -10 < x < +10"      
-           STOP RUN
+           DISPLAY "Error: number x must be -10 < x < +10"
            END-IF
 
            IF A = 0 THEN
-           DISPLAY "Result: Infinite"           
-           STOP RUN
+           DISPLAY "Result: Infinite"
            END-IF
 
       *> Initialize the algorithm 
@@ -33,13 +31,13 @@
            MOVE +0.01 TO XOLD
            END-IF  
 
-      *> Use the algorithm 
-           PERFORM NRinv N TIMES
-           DIVIDE  A INTO 1 GIVING INV
-           DISPLAY "Exact Result:" INV
-           STOP RUN.
-
-           NRinv.
-           COMPUTE XNEW ROUNDED = XOLD *( 2 - A * XOLD)
-           DISPLAY "Result: ",XNEW
-           MOVE XNEW TO XOLD.
+      *> Use the algorithm
+           IF (NOT A = 0) AND A > -10 AND A <= 10 THEN
+               PERFORM N TIMES
+                   COMPUTE XNEW = XOLD *( 2 - A * XOLD)
+                   DISPLAY "Result: ",XNEW
+                   MOVE XNEW TO XOLD
+               END-PERFORM
+               DIVIDE  A INTO 1 GIVING INV
+               DISPLAY "Exact Result:" INV
+           END-IF.
--- 003/ComplexData.cbl
+++ 003/ComplexData.cbl
@@ -13,7 +13,7 @@
                    03 B     PIC 99.
                    03 C     PIC 9(4).
                02 city      PIC X(10).
-           01 n_letters     PIC 9.
+           01 n-letters     PIC 9.
 
        PROCEDURE DIVISION.           
       *> Set the values of the fields.
@@ -23,14 +23,14 @@
       
       *> This works
            MOVE "Michael   Smith     123456789Portland" TO Employee
-           DISPLAY fname "," SPACE lname " SSN: "A "-" B "-" C
-      
+           DISPLAY fname ", " lname " SSN: "A "-" B "-" C
+
       *> This works, too
            MOVE "Michael" TO fname
            MOVE "Smith" TO lname
            MOVE "123456789" TO SSN
            MOVE "Portland" TO city
-           DISPLAY fname "," SPACE lname " SSN: "A "-" B "-" C
+           DISPLAY fname ", " lname " SSN: "A "-" B "-" C
 
       *> Another way
            MOVE "Michael" TO fname
@@ -39,17 +39,4 @@
            MOVE "45"   TO B
            MOVE "6789" TO C
            MOVE "Portland" TO city
-           DISPLAY fname "," SPACE lname " SSN: "A "-" B "-" C
-           
-      *> As a "vector"
-           MOVE "Michael   Smith     123456789Portland" TO Employee
-           DISPLAY Employee(1:10) "," SPACE Employee(11:10) 
-               " SSN: " Employee(21:3) "-" Employee(24:2) "-" 
-                   Employee(26:4).
-
-      *> NOTE: if you want to remove the trailing spaces:
-           INSPECT fname TALLYING n_letters FOR CHARACTERS BEFORE SPACE
-           DISPLAY fname(1:n_letters) "," SPACE lname 
-               " SSN: "A "-" B "-" C.
-           STOP RUN.
-
+           DISPLAY fname ", " lname " SSN: "A "-" B "-" C.
